25f05e016242352d2a55598c68d016c8f63e6bf3
Gaurav <gauravgidye26@gmail.com>
2022-09-12 18:22:07 +0530

    First Commit by Gaurav

--- src/main/java/DemoClass.java
+++ src/main/java/DemoClass.java
@@ -1,6 +1,6 @@
 public class DemoClass {
     public static void main(String args[])
     {
-        System.out.println("Welcome to MavenDemo");
+        System.out.println("Welcome to MavenDemo 1");
     }
 }
